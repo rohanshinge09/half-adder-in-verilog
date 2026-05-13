20f0cd850a5cc2cd1886b0ce729792ba297446b4
Rohan Shinge <rohanshinge0528@gmail.com>
2026-05-13 22:08:08 +0530

    Update half_adder_tb.v

--- half_adder_tb.v
+++ half_adder_tb.v
@@ -7,16 +7,22 @@ wire sum, carry;
 
 half_adder dut(a, b, sum, carry);
 
-initial begin
-    $monitor("Time=%0t | a=%b b=%b | sum=%b carry=%b",
-              $time, a, b, sum, carry);
-    
-    a = 0; b = 0; #10;
-    a = 0; b = 1; #10;
-    a = 1; b = 0; #10;
-    a = 1; b = 1; #10;
+initial 
+    begin
 
-    $finish;
+    a = 0;
+    b = 0;
+    #10;
+    a = 0;
+    b = 1;
+    #10;
+    a = 1;
+    b = 0;
+    #10;
+    a = 1;
+    b = 1;
+    #10;
+
 end
 
 endmodule
